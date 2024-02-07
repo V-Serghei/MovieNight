e026daadaee6bf251ef6855898bd23d5244aadce
Richie_Vist <vistovschiiserhei@gmail.com>
2024-02-07 12:53:56 +0200

    connection, bug fix

--- MovieNight.Web/scripts/vendor.js
+++ MovieNight.Web/scripts/vendor.js
@@ -17,13 +17,13 @@
 
     if (typeof module === "object" && typeof module.exports === "object") {
 
-        // For CommonJS and CommonJS-like environments where a proper `window`
-        // is present, execute the factory and get jQuery.
-        // For environments that do not have a `window` with a `document`
-        // (such as Node.js), expose a factory as module.exports.
-        // This accentuates the need for the creation of a real `window`.
-        // e.g. var jQuery = require("jquery")(window);
-        // See ticket #14549 for more info.
+         //For CommonJS and CommonJS-like environments where a proper `window`
+         //is present, execute the factory and get jQuery.
+         //For environments that do not have a `window` with a `document`
+         //(such as Node.js), expose a factory as module.exports.
+         //This accentuates the need for the creation of a real `window`.
+         //e.g. var jQuery = require("jquery")(window);
+         //See ticket #14549 for more info.
         module.exports = global.document ?
             factory(global, true) :
             function (w) {
