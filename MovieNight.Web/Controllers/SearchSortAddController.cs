a02c6d423126ae3818d220a1bdf16474666fb226
Nelea-Lora <sceastlivaianelea@gmail.com>
2024-02-15 16:45:59 +0200

    Add friends page

---
+++ MovieNight.Web/Controllers/SearchSortAddController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNight.Web.Controllers
+{
+    public class SearchSortAddController : Controller
+    {
+        // GET: SearchSortAdd
+        public ActionResult FriendsPage()
+        {
+            return View();
+        }
+    }
+}
