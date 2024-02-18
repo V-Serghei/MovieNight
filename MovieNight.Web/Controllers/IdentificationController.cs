8fbf806c67608f28ea148b0e76c9eda7d328b40a
Nelea-Lora <sceastlivaianelea@gmail.com>
2024-02-18 12:57:58 +0200

    Login, register, calendar

---
+++ MovieNight.Web/Controllers/IdentificationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNight.Web.Controllers
+{
+    public class IdentificationController : Controller
+    {
+        // GET: Identification
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
