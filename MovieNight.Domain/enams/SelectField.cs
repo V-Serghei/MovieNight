b5ef46d2f0ccc1a52d6c3a7d4f6cd12e1b25ded8
V-Serghei <vistovschiiserghei@gmail.com>
2024-04-20 00:47:27 +0300

    Intermediate result, sorting and filtering

---
+++ MovieNight.Domain/enams/SelectField.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieNight.Domain.enams
+{
+    public enum SelectField
+    {
+        Non = 0,
+        Title,
+        YearOfRelease,
+        ReviewDate,
+        MovieNight,
+        UserValues,
+    }
+}
